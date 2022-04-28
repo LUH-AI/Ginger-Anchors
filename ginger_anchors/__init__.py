6b0163ba201ee97b6c142c81c6aa2da9c1d339bb
René Sass <mail@renesass.de>
2022-04-28 10:02:14 +0200

    Fixed url

--- ginger_anchors/__init__.py
+++ ginger_anchors/__init__.py
@@ -5,9 +5,9 @@ package_name = "ginger_anchors"
 author = "Jim Rhotert and Julian Bilsky"
 author_email = "rhotert@stud.uni-hannover.de"
 description = "Reimplementation of Anchors: High-Precision Model-Agnostic Explanations."
-url = "www.automl.org"
+url = "https://www.automl.org"
 project_urls = {
     "Source Code": "https://github.com/LUH-AI/ginger_anchors",
 }
-copyright = f"Copyright {datetime.date.today().strftime('%Y')}, Jim Rhotert and Julian Bilsky, <<author_email>>"
+copyright = f"Copyright {datetime.date.today().strftime('%Y')}, Jim Rhotert and Julian Bilsky"
 version = "0.1"
